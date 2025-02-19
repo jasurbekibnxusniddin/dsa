fc1bde79f30444f119239ca3aeed1cfe40e51a16
jasurbek <jasurbekibnxusniddin@gmail.com>
2025-02-19 21:08:09 +0500

    1415

--- 2_algorithms/leetcode/1415.go
+++ 2_algorithms/leetcode/1415.go
@@ -3,13 +3,36 @@ package leetcode
 import "fmt"
 
 func getHappyString(n int, k int) string {
+	var (
+		backtrack func() bool
+		seq       = make([]rune, 0, n)
+	)
 
-	var str string
+	backtrack = func() bool {
+		if len(seq) == n {
+			k--
+			if k == 0 {
+				return true
+			}
+			return false
+		}
 
-	fmt.Println(n, k)
-
-	return str
+		for choice := 'a'; choice <= 'c'; choice++ {
+			if len(seq) > 0 && seq[len(seq)-1] == choice {
+				continue
+			}
+			seq = append(seq, choice)
+			if backtrack() {
+				return true
+			}
+			seq = seq[:len(seq)-1]
+		}
+		return false
+	}
+	backtrack()
+	return string(seq)
 }
+        
 
 func GetHappyString() {
 
